4c953da8bc2b6004d67d6350d6ea771a200a76a3
Anna Francisco <franc308@msu.edu>
2024-09-16 15:51:48 -0400

    Update WorkedProblem2.py

--- WorkedProblem2.py
+++ WorkedProblem2.py
@@ -43,17 +43,11 @@ plt.title('Phase Space Diagram')
 
 
 # In[52]:
-
-
-from scipy.integrate import solve_ivp
-import numpy as np
-import matplotlib.pyplot as plt
-
 k1 = 0.5  # cooling constant for object
 A = 1  # amplitude of temperature oscillation
 omega = 0.1  # frequency of oscillation
 
-def system(t, y):
+def system2(t, y):
     T_obj, T_env = y
     dT_obj_dt = -k1 * (T_obj - T_env)  # cooling of object 1
     dT_env_dt = A * np.sin(omega * t)  # oscillating ambient temperature
@@ -66,7 +60,7 @@ y0 = [T_obj_0, T_env_0]
 t_span = (0, 80)
 t_eval = np.linspace(t_span[0], t_span[1], 500)
 
-sol = solve_ivp(system, t_span, y0, t_eval=t_eval)
+sol = solve_ivp(system2, t_span, y0, t_eval=t_eval)
 
 plt.plot(sol.t, sol.y[0], label='Object Temperature')
 plt.plot(sol.t, sol.y[1], label='Ambient Temperature', linestyle='dotted')
@@ -88,18 +82,13 @@ plt.show()
 
 # In[50]:
 
-
-from scipy.integrate import solve_ivp
-import numpy as np
-import matplotlib.pyplot as plt
-
 k1 = 0.5  # cooling constant for object 1
 k2 = 0.4  # cooling constant for object 2
 alpha = 0.1  # heat exchange rate between object 1 and object 2
 A = 5 # amplitude of ambient temperature oscillation
 omega = 0.8  # frequency of oscillation
 
-def system(t, y):
+def system3(t, y):
     T1, T2, T_env = y
     dT1_dt = -k1 * (T1 - T_env) - alpha * (T1 - T2)
     dT2_dt = -k2 * (T2 - T_env) + alpha * (T1 - T2)
@@ -109,7 +98,7 @@ def system(t, y):
 t_span = (0, 10) 
 y0 = [50,80,70]  # temperatures of T1, T2, and T_env
 
-sol = solve_ivp(system, t_span, y0, t_eval=np.linspace(t_span[0], t_span[1], 500))
+sol = solve_ivp(system3, t_span, y0, t_eval=np.linspace(t_span[0], t_span[1], 500))
 
 plt.figure(figsize=(10, 6))
 plt.plot(sol.t, sol.y[0], label='Temperature of Object 1', color='purple')
